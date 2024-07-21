f7b08257f68ddfaeb6a1dfee858670fbbeed70c1
ShivamVatsAs <shivamchy867@gmail.com>
2024-07-21 22:35:32 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+// 霸都丶傲天 2019/7/9  https://Github.com/AJLoveChina
+var config = {
+    // 下面的句子不一定非要7句， 你也可以改成4句，2句话都可以
+    lines: [
+        "Like petals falling gently,",
+        "our love story unfolds, each moment a precious memory. ",
+        "Time may tick away,",
+        "but our affection grows ever richer,",
+        "a treasure we cherish with every beat of our hearts💖",
+    ],
+    // 相爱的时间 记住格式不能写错了, 非常重要
+    // 格式一定要和下面这个时间格式一样
+    // 分别是 年-月-日T时时:分分:秒秒
+    //      温馨警告:
+    //          1.时分秒都必须是俩个数字  比如 00:00:00
+    //          2.T不能丢了
+    date: "2024-06-02T00:00:00",
+
+
+    names: ["Ashni", "🐝Shivam"]
+}
